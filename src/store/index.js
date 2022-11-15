0354198cdceb66b3493908367d470fe6904b5520
totix2 <x2app123@gmail.com>
2022-11-15 16:52:09 +0500

    c

--- src/store/index.js
+++ src/store/index.js
@@ -3,21 +3,10 @@ import {User} from './user/index';
 import {Userv} from './userv/index';
 import {Trips} from './trips/index';
 
-import {Orders} from './orders/index';
-import {Promos} from './promos/index';
-import {Resturants} from './resturants';
 import {Search} from './search';
 import {Filters} from './filters';
 import {Notifications} from './notifications';
 import {Offers} from './offers';
-
-// import notificationmanager from "./notificationmanager";
-
-// const userStore = new userstore();
-// const carStore = new carstore();
-// const cityStore = new citystore();
-// const tripStore = new tripstore();
-// const NotificationManager = new notificationmanager();
 
 export default {
   General,
