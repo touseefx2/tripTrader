ba0a1ac4b14e6109fd022bb04d69e70899fe0a78
totix2 <x2app123@gmail.com>
2022-11-16 11:50:29 +0500

    c

--- src/database/apis/index.js
+++ src/database/apis/index.js
@@ -88,7 +88,7 @@ const GET_RECEIVED_OFFERS = 'api/offer?offeredTo=';
 
 const CANCEL_OFFER = 'api/offer/declineTrip/';
 const CONFIRM_OFFERS = 'api/offer/confirmTrip/';
-const GET_CONFIRM_OFFERS = 'api/offer?';
+const GET_CONFIRM_OFFERS = 'api/offer/getConfirmedTrips/';
 
 export default apis = {
   BASE_URL,
--- src/store/offers/index.js
+++ src/store/offers/index.js
@@ -137,8 +137,8 @@ class offers {
     this.setLoader3(true);
 
     let token = store.User.authToken;
-    let uid = store.User.user._id;
-    let params = `offeredBy=${uid}&offeredTo=${uid}&status=confirmed`;
+    let params = store.User.user._id;
+    // let params = `offeredBy=${uid}&offeredTo=${uid}&status=confirmed`;
     db.hitApi(db.apis.GET_CONFIRM_OFFERS + params, 'get', {}, token)
       ?.then(resp => {
         this.setLoader3(false);
