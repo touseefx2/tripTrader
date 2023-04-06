5b72fb6a746837d5b50445275ec1e64d26c9479a
totix2 <x2app123@gmail.com>
2023-04-06 17:04:18 +0500

    firebaseChat

--- src/utils/MessageModal/index.js
+++ src/utils/MessageModal/index.js
@@ -16,7 +16,7 @@ import Header from './components/Header';
 import Bottom from './components/Bottom';
 import ProgressiveFastImage from '@freakycoder/react-native-progressive-fast-image';
 import NetInfo from '@react-native-community/netinfo';
-// import firestore from '@react-native-firebase/firestore';
+import firestore from '@react-native-firebase/firestore';
 
 export default function MessageModal({
   isModal,
@@ -32,11 +32,11 @@ export default function MessageModal({
   const {item} = modalObj;
   const {_id, firstName, lastName, image} = item.hostId;
 
-  // const {user} = store.User;
-  // const userId1 = user._id;
-  // const userId2 = item.hostId._id;
-  // const senderName = user.firstName + ' ' + user.lastName;
-  // const senderImage = user.image || '';
+  const {user} = store.User;
+  const userId1 = user._id;
+  const userId2 = item.hostId._id;
+  const senderName = user.firstName + ' ' + user.lastName;
+  const senderImage = user.image || '';
 
   const [isMaxHeight, setIssMaxHeight] = useState(false);
   const [modalHeight, setmodalHeight] = useState(0);
@@ -121,148 +121,147 @@ export default function MessageModal({
     setSuccessCheck('MessageSend');
     setIsSuccessModal(true);
   };
-  const sendMessage = () => {
-    Keyboard.dismiss();
-    NetInfo.fetch().then(state => {
-      if (state.isConnected) {
-        const {user} = store.User;
-        const senderId = user._id;
-        const senderName = user.firstName + ' ' + user.lastName;
-        const senderImage = user.image || '';
-
-        const obj = {
-          userId1: senderId,
-          userId2: _id,
-          sendBy: senderId,
-          sendTo: _id,
-          senderName: senderName,
-          isRead: false,
-          message: message,
-          type: 'text',
-          senderImage: senderImage,
-        };
-
-        store.User.attemptToCheckFirstMessage(
-          senderId,
-          _id,
-          obj,
-          message,
-          messageSuccefullySend,
-        );
-      } else {
-        Alert.alert('', 'Please connect internet');
-      }
-    });
-  };
-
-  // const chatroomsRef = firestore().collection('chatrooms');
   // const sendMessage = () => {
   //   Keyboard.dismiss();
-  //   NetInfo.fetch().then(async state => {
+  //   NetInfo.fetch().then(state => {
   //     if (state.isConnected) {
+  //       const {user} = store.User;
+  //       const senderId = user._id;
+  //       const senderName = user.firstName + ' ' + user.lastName;
+  //       const senderImage = user.image || '';
+
   //       const obj = {
-  //         latestMessage: null,
-  //         userId1: user,
-  //         userId2: item.hostId,
-  //         createdAt: Date.now(),
-  //         updatedAt: Date.now(),
+  //         userId1: senderId,
+  //         userId2: _id,
+  //         sendBy: senderId,
+  //         sendTo: _id,
+  //         senderName: senderName,
+  //         isRead: false,
+  //         message: message,
+  //         type: 'text',
+  //         senderImage: senderImage,
   //       };
-  //       const chatRoom = await getChatRoom();
-  //       console.log('chatRoom : ', chatRoom);
-  //       if (typeof chatRoom == 'string') {
-  //         Alert.alert('Error', chatRoom);
-  //         return;
-  //       }
-  //       const messageData = chatRoom.find(
-  //         item =>
-  //           (item.userId1._id == userId1 && item.userId2._id == userId2) ||
-  //           (item.userId1._id == userId2 && item.userId2._id == userId1),
+
+  //       store.User.attemptToCheckFirstMessage(
+  //         senderId,
+  //         _id,
+  //         obj,
+  //         message,
+  //         messageSuccefullySend,
   //       );
-  //       console.log('messageData : ', messageData);
-  //       if (messageData) {
-  //         sendChatMessage(messageData._id);
-  //         return;
-  //       }
-  //       createChatRoom(obj);
   //     } else {
   //       Alert.alert('', 'Please connect internet');
   //     }
   //   });
   // };
 
-  // function getChatRoom() {
-  //   return new Promise((resolve, reject) => {
-  //     chatroomsRef
-  //       .get()
-  //       .then(querySnapshot => {
-  //         let data = [];
-  //         if (querySnapshot.size > 0) {
-  //           data = querySnapshot.docs.map(doc => ({
-  //             ...doc.data(),
-  //             _id: doc.id,
-  //           }));
-  //         }
-  //         resolve(data);
-  //       })
-  //       .catch(error => {
-  //         resolve(error);
-  //       });
-  //   });
-  // }
+  const chatroomsRef = firestore().collection('chatrooms');
+  const sendMessage = () => {
+    Keyboard.dismiss();
+    NetInfo.fetch().then(async state => {
+      if (state.isConnected) {
+        const obj = {
+          latestMessage: null,
+          userId1: user,
+          userId2: item.hostId,
+          createdAt: Date.now(),
+          updatedAt: Date.now(),
+        };
+        const chatRoomArr = await getChatRoom();
+        console.log('chatRoomArr : ', chatRoomArr);
+        const room = chatRoomArr.find(
+          item =>
+            (item.userId1._id == userId1 && item.userId2._id == userId2) ||
+            (item.userId1._id == userId2 && item.userId2._id == userId1),
+        );
+        console.log('room : ', room);
+        if (room) {
+          sendChatMessage(room._id);
+          return;
+        }
+        createChatRoom(obj);
+      } else {
+        Alert.alert('', 'Please connect internet');
+      }
+    });
+  };
 
-  // function createChatRoom(obj) {
-  //   chatroomsRef
-  //     .add(obj)
-  //     .then(res => {
-  //       console.log('ChatRoom Created: ');
-  //       sendChatMessage(res.id);
-  //     })
-  //     .catch(error => {
-  //       console.log('Error createChatRoom:', error);
-  //       Alert.alert('Error', error);
-  //     });
-  // }
+  function getChatRoom() {
+    return new Promise((resolve, reject) => {
+      chatroomsRef
+        .get()
+        .then(querySnapshot => {
+          let data = [];
+          if (querySnapshot.size > 0) {
+            data = querySnapshot.docs.map(doc => ({
+              ...doc.data(),
+              _id: doc.id,
+            }));
+          }
+          resolve(data);
+        })
+        .catch(error => {
+          console.log('Error getChatRoom:', error);
+          reject(error?.message || error);
+          Alert.alert('Error', error?.message || error);
+        });
+    });
+  }
 
-  // function sendChatMessage(roomId) {
-  //   const obj = {
-  //     sendBy: userId1,
-  //     sendTo: userId2,
-  //     senderName: senderName,
-  //     isRead: false,
-  //     message: message,
-  //     type: 'text',
-  //     senderImage: senderImage,
-  //     deletedBy: [],
-  //     createdAt: Date.now(),
-  //     updatedAt: Date.now(),
-  //   };
-  //   chatroomsRef
-  //     .doc(roomId)
-  //     .collection('messages')
-  //     .add(obj)
-  //     .then(res => {
-  //       console.log('sendChatMessage Success: ');
-  //       updateLatestMessageinRoom(roomId, obj);
-  //     })
-  //     .catch(error => {
-  //       console.log('Error createChatRoom:', error);
-  //       Alert.alert('Error', error);
-  //     });
-  // }
+  function createChatRoom(obj) {
+    chatroomsRef
+      .add(obj)
+      .then(res => {
+        console.log('ChatRoom Created: ');
+        sendChatMessage(res.id);
+      })
+      .catch(error => {
+        console.log('Error createChatRoom:', error);
+        Alert.alert('Error', error?.message || error);
+      });
+  }
 
-  // function updateLatestMessageinRoom(roomId, obj) {
-  //   chatroomsRef
-  //     .doc(roomId)
-  //     .update({
-  //       latestMessage: obj,
-  //     })
-  //     .then(() => {
-  //       console.log('LatestMessageinRoom updated!');
-  //     })
-  //     .catch(error => {
-  //       console.log('Error updateLatestMessageinRoom:', error);
-  //     });
-  // }
+  function sendChatMessage(roomId) {
+    const obj = {
+      sendBy: userId1,
+      sendTo: userId2,
+      senderName: senderName,
+      isRead: false,
+      message: message,
+      type: 'text',
+      senderImage: senderImage,
+      deletedBy: [],
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+    chatroomsRef
+      .doc(roomId)
+      .collection('messages')
+      .add(obj)
+      .then(res => {
+        console.log('sendChatMessage Success: ');
+        updateLatestMessageinRoom(roomId, obj);
+      })
+      .catch(error => {
+        console.log('Error sendChatMessage:', error);
+        Alert.alert('Error', error?.message || error);
+      });
+  }
+
+  function updateLatestMessageinRoom(roomId, obj) {
+    chatroomsRef
+      .doc(roomId)
+      .update({
+        latestMessage: obj,
+      })
+      .then(() => {
+        console.log('LatestMessageinRoom updated!');
+      })
+      .catch(error => {
+        console.log('Error updateLatestMessageinRoom:', error);
+        Alert.alert('Error', error?.message || error);
+      });
+  }
 
   return (
     <Modal visible={isModal} transparent onRequestClose={closeModal}>
