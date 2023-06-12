19417bcba4fbe0316b51c17ebb5649bc8928f7d5
farahtahir22 <ftahir.bscs15seecs@seecs.edu.pk>
2023-06-13 02:46:57 +0500

    iOS Update

--- src/store/general/index.js
+++ src/store/general/index.js
@@ -11,6 +11,7 @@ class general {
 
   @observable Stripe_Publish_Key =
     'pk_test_51M9HIuBmhbfqULZ4IstWDtc73GFl6mVRnA4jUcOR9BVRkndz1Ou2FSlOeP4WjGgYqlH4LflMtgUY8foGkY58lHAq00OGfQUjlR';
+    // pk_live_51M9HIuBmhbfqULZ4zLBVSrEMd8hpXSfnEcUQvRnZHdPfkzKy5mXtLINeAaglb9ZoIg3muBImsh2X6Q01b8nylFXU00SiTzkpZp
   @observable Terms_and_Conditions_Link =
     'http://trip-trader.com.s3-website-us-east-1.amazonaws.com/termsandconditionsapp';
   @observable Privacy_and_Policy_Link =
