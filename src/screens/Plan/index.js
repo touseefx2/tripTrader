c6502d1f89639f0da81ded51fbe9ebad9246209f
totix2 <x2app123@gmail.com>
2023-08-12 19:34:01 +0500

    strine pymnt mthd > 0.5

--- src/screens/Plan/index.js
+++ src/screens/Plan/index.js
@@ -266,8 +266,8 @@ function Plan(props) {
   const SucGetClientsecret = async (dt, obj) => {
     try {
       const { error, paymentIntent } = await confirmPayment(dt.cs, {
-        // paymentMethodType: "Card", //strip > 0.5.0
-        type: "Card", //stripe <= 0.5.0
+        paymentMethodType: "Card", //strip > 0.5.0
+        // type: "Card", //stripe <= 0.5.0
         billingDetails: { name: cfn.trim() },
       });
 
